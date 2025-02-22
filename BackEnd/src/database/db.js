32722d5e4086956418f93a40fda83a11da07a1be
Arifkhan2007111 <arifkhan2007111@gmail.com>
2025-02-22 19:18:17 +0530

    db changes

--- BackEnd/src/database/db.js
+++ BackEnd/src/database/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Connection = async ()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL)
+        console.log(process.env.MONGO_URL)
         console.log("Database Connected")
     }catch(error){
         console.log("Error", error)
